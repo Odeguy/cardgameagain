88c865a2affbc43fb7498bb88a02cc460a975592
Odeguy <lereoatschool@gmail.com>
2025-04-08 20:46:56 -0400

    card stuff

--- card_script.gd
+++ card_script.gd
@@ -2,3 +2,16 @@ extends Node2D
 
 var card_name
 var effect
+var front = false
+
+func _ready():
+	toggle_side()
+
+func toggle_side():
+	if(front):
+		$Body/Back.show()
+		$Body/Front.hide()
+	else:
+		$Body/Back.hide()
+		$Body/Front.show()
+	front = !front
