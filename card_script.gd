822a47c8c69cac644ec4937c09c8a8eeb86bddb2
Odeguy <lereoatschool@gmail.com>
2025-05-13 10:04:05 -0400

    started changing card

--- card_script.gd
+++ card_script.gd
@@ -3,7 +3,8 @@ extends Node2D
 var card_name
 var effect
 var front = false
-var color
+var back_color
+var font_color
 var font
 var font_size
 var text_time = 1
@@ -16,17 +17,10 @@ func _ready():
 	load_text(true)
 
 func switch_side(side: String):
-	if side == "back":
-		$Body/Back.show()
-		$Body/Front.hide()
-		front = false
-	elif side == "front":
-		$Body/Back.hide()
-		$Body/Front.show()
-		front = true
+	pass
 	
-func set_color(color_entered: Color):
-	color = color_entered
+func set_font_color(font_color_entered: Color):
+	font_color = font_color_entered
 	re_push()
 
 func set_font_size(size_entered: int):
@@ -50,20 +44,20 @@ func set_effect(effect_entered: String):
 	re_push()
 
 func re_push():
-	$Body/Front/NameLabel.clear()
-	if(color != null): $Body/Front/NameLabel.push_color(color)
-	if(font_size != null): $Body/Front/NameLabel.push_font_size(font_size)
-	if(font != null): $Body/Front/NameLabel.push_font(font)
+	$Body/BackNameLabel.clear()
+	if(font_color != null): $Body/BackNameLabel.push_font_color(font_color)
+	if(font_size != null): $Body/BackNameLabel.push_font_size(font_size)
+	if(font != null): $Body/BackNameLabel.push_font(font)
 	
-	$Body/Front/EffectLabel.clear()
-	if(color != null): $Body/Front/EffectLabel.push_color(color)
-	if(font_size != null): $Body/Front/EffectLabel.push_font_size(font_size)
-	if(font != null): $Body/Front/EffectLabel.push_font(font)
+	$Body/BackEffectLabel.clear()
+	if(font_color != null): $Body/BackEffectLabel.push_font_color(font_color)
+	if(font_size != null): $Body/BackEffectLabel.push_font_size(font_size)
+	if(font != null): $Body/BackEffectLabel.push_font(font)
 	
-	$Body/Front/PointsLabel.clear()
-	if(color != null): $Body/Front/PointsLabel.push_color(color)
-	if(font_size != null): $Body/Front/PointsLabel.push_font_size(font_size * 3)
-	if(font != null): $Body/Front/PointsLabel.push_font(font)
+	$Body/BackPointsLabel.clear()
+	if(font_color != null): $Body/BackPointsLabel.push_font_color(font_color)
+	if(font_size != null): $Body/BackPointsLabel.push_font_size(font_size * 3)
+	if(font != null): $Body/BackPointsLabel.push_font(font)
 	
 func get_size() -> Vector2:
 	return $Body/Shape.get_shape().size
@@ -74,8 +68,11 @@ func get_card_name() -> String:
 func get_effect() -> String:
 	return effect
 
-func get_color() -> Color:
-	return color
+func get_font_color() -> Color:
+	return font_color
+	
+func get_back_color() -> Color:
+	return back_color
 	
 func get_font() -> String:
 	return font
@@ -86,13 +83,10 @@ func get_font_size() -> int:
 func get_points() -> int:
 	return points
 
-func has_point(point: Vector2) -> bool:
-	return $Body/Shape.shape.get_rect().has_point(to_local(point))
-	
 func load_text(gradually):
-	$Body/Front/NameLabel.clear()
-	$Body/Front/EffectLabel.clear()
-	$Body/Front/PointsLabel.clear()
+	$Body/BackNameLabel.clear()
+	$Body/BackEffectLabel.clear()
+	$Body/BackPointsLabel.clear()
 	re_push()
 	var effect_with_int = effect #effect but [points value] is replaced with the actual number
 	while effect_with_int.find("[points value]") != -1:
@@ -100,13 +94,14 @@ func load_text(gradually):
 	if gradually:
 		loaded = false
 		for char in card_name:
-			$Body/Front/NameLabel.append_text(char)
+			$Body/BackNameLabel.append_text(char)
 			await get_tree().create_timer(text_time / card_name.length()).timeout
 		for char in effect_with_int:
-			$Body/Front/EffectLabel.append_text(char)
+			$Body/BackEffectLabel.append_text(char)
 			await get_tree().create_timer(text_time / effect.length()).timeout
 	else:
-		$Body/Front/NameLabel.append_text(card_name)
-		$Body/Front/EffectLabel.append_text(effect_with_int)
-	$Body/Front/PointsLabel.append_text(str(points))
+		$Body/BackNameLabel.append_text(card_name)
+		$Body/BackEffectLabel.append_text(effect_with_int)
+	$Body/BackPointsLabel.append_text(str(points))
 	loaded = true
+	
